e206389b4c646bcd28d185cdb4fb985443e82a67
Hidenori Miyoshi <miyohide@gmail.com>
2023-12-21 21:09:41 +0900

    access postgresql database

--- lambda_function.rb
+++ lambda_function.rb
@@ -1,6 +1,20 @@
+require 'pg'
+
 module LambdaFunction
   class Handler
     def self.process(event:,context:)
+      conn = PG.connect(
+        host: ENV['PG_HOSTNAME'],
+        port: 5432,
+        user: ENV['PG_USERNAME'],
+        password: ENV['PG_PASSWORD']
+      )
+      conn.exec("SELECT * FROM pg_stat_activity") do |result|
+        puts "     PID | User             | Query"
+        result.each do |row|
+          puts " %7d | %-16s | %s " % row.values_at('pid', 'username', 'query')
+        end
+      end
       "Hello from Lambda!"
     end
   end
