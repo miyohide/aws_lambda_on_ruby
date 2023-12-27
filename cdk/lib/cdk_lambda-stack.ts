1700e7616796d8426a07e3ea7c9b6e097c3404d7
Hidenori Miyoshi <miyohide@gmail.com>
2023-12-27 11:55:31 +0900

    create docker image lambda

---
+++ cdk/lib/cdk_lambda-stack.ts
@@ -0,0 +1,16 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Repository } from "aws-cdk-lib/aws-ecr";
+import { DockerImageCode, DockerImageFunction } from "aws-cdk-lib/aws-lambda";
+
+export class CdkLambdaStack extends Stack {
+    // Lambda関数を生成する
+    constructor(scope: Stack, id: string, props?: StackProps) {
+        super(scope, id, props);
+
+        const repository = Repository.fromRepositoryName(this, "Repository", "my-ruby-app");
+        // CDKでDockerイメージを使ったLambda関数を作成する
+        new DockerImageFunction(this, "LambdaFunction", {
+            code: DockerImageCode.fromEcr(repository, { tag: "latest" }),
+        });
+    }
+}
