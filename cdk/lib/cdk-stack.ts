a485ef3427070ca2f889d70aa744b363bc6154c9
Hidenori Miyoshi <miyohide@gmail.com>
2023-12-28 21:00:13 +0900

    update cdk stack

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,35 @@
 import * as cdk from 'aws-cdk-lib';
+import { SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // VPCの作成
+    const vpc = new Vpc(this, "MyVPC", {
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+      maxAzs: 2,
+      natGateways: 1,
+      subnetConfiguration: [
+        {
+          name: "PublicSubnet",
+          subnetType: SubnetType.PUBLIC,
+          cidrMask: 24,
+          mapPublicIpOnLaunch: true,
+        },
+        {
+          name: "PrivateSubnet",
+          subnetType: SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 24,
+        },
+        {
+          name: "DBSubnet",
+          subnetType: SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24,
+        }
+      ]
+    });
   }
 }
