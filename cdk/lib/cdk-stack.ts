c6ca89b1f0b8cb32239cdd47d59c78dd50689a71
Hidenori Miyoshi <miyohide@gmail.com>
2023-12-30 13:07:54 +0900

    create ec2 instance

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
-import { InstanceClass, InstanceSize, InstanceType, Port, SecurityGroup, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
+import { AmazonLinuxImage, Instance, InstanceClass, InstanceSize, InstanceType, Port, SecurityGroup, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { DatabaseInstance, DatabaseInstanceEngine, SubnetGroup } from 'aws-cdk-lib/aws-rds';
 import { Construct } from 'constructs';
 
@@ -33,6 +33,24 @@ export class CdkStack extends cdk.Stack {
       ]
     });
 
+    // EC2用のセキュリティグループを作成する
+    const ec2SecurityGroup = new SecurityGroup(this, "EC2SecurityGroup", {
+      vpc: vpc,
+      description: "EC2 Security Group",
+    });
+
+    // Databaseマネジメント用のEC2インスタンスを作成する
+    const machineImage = new AmazonLinuxImage();
+    const ec2Instance = new Instance(this, "ec2Instance", {
+      vpc: vpc,
+      instanceType: InstanceType.of(InstanceClass.T2, InstanceSize.MICRO),
+      machineImage: machineImage,
+      vpcSubnets: {
+        subnetType: SubnetType.PRIVATE_WITH_EGRESS,
+      },
+      securityGroup: ec2SecurityGroup,
+    })
+
     // RDS用のセキュリティグループの作成
     const rdsSecurityGroup = new SecurityGroup(this, "RDSSecurityGroup", {
       vpc: vpc,
@@ -60,5 +78,7 @@ export class CdkStack extends cdk.Stack {
       subnetGroup: dbSubnetGroup,
       securityGroups: [rdsSecurityGroup],
     });
+
+    rdsInstance.connections.allowDefaultPortFrom(ec2Instance, "EC2 to RDS");
   }
 }
