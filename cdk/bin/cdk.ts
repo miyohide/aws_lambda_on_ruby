9148fe8001785693bea725f5579307c506efa68e
Hidenori Miyoshi <miyohide@gmail.com>
2023-12-30 20:15:01 +0900

    create CdkEcrStack

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -2,6 +2,8 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CdkStack } from '../lib/cdk-stack';
+import { CdkEcrStack } from '../lib/cdk_ecr-stack';
 
 const app = new cdk.App();
+new CdkEcrStack(app, 'ECRStack', {});
 new CdkStack(app, 'CdkStack', {});
