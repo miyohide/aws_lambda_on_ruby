e6007a6021fbf1d2c96cc7ee869cef8fb2f2e4c8
Hidenori Miyoshi <miyohide@gmail.com>
2024-01-05 20:34:55 +0900

    add codestack

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,7 +3,9 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CdkStack } from '../lib/cdk-stack';
 import { CdkEcrStack } from '../lib/cdk_ecr-stack';
+import { CdkCodesStack } from '../lib/cdk_codes-stack';
 
 const app = new cdk.App();
 new CdkEcrStack(app, 'ECRStack', {});
+new CdkCodesStack(app, 'CodesStack', {});
 new CdkStack(app, 'CdkStack', {});
