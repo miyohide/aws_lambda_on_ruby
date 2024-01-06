879e31e0c8cbe3d0402dd7ba962643677087282f
Hidenori Miyoshi <miyohide@gmail.com>
2024-01-06 20:10:56 +0900

    add ECR test

--- cdk/test/cdk.test.ts
+++ cdk/test/cdk.test.ts
@@ -1,17 +1,21 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Cdk from '../lib/cdk-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { CdkEcrStack } from '../lib/cdk_ecr-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Cdk.CdkStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('ECRが1つ作成されること', () => {
+  const app = new cdk.App();
+  const stack = new CdkEcrStack(app, 'ECRStack');
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  // 作成されるECRの数を確認する
+  template.resourceCountIs('AWS::ECR::Repository', 1);
+  // 作成されるECRがもつCloudFormationのPropertiesを確認する
+  template.hasResourceProperties('AWS::ECR::Repository', {
+    RepositoryName: 'my-ruby-app',
+    EmptyOnDelete: true,  // 中身があった場合でも削除する
+  });
+  // 作成されるECRのProperties以外の値を確認する
+  template.hasResource('AWS::ECR::Repository', {
+    DeletionPolicy: 'Delete', // スタック削除時にECRも削除する
+  });
 });
