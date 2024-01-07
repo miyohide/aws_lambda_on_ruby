40c490160979d8186871cee5afe7f9698212ccb8
Hidenori Miyoshi <miyohide@gmail.com>
2024-01-07 20:41:10 +0900

    surround describe

--- cdk/test/cdk.test.ts
+++ cdk/test/cdk.test.ts
@@ -1,21 +1,23 @@
-import * as cdk from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
-import { CdkEcrStack } from '../lib/cdk_ecr-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { CdkEcrStack } from "../lib/cdk_ecr-stack";
 
-test('ECRが1つ作成されること', () => {
-  const app = new cdk.App();
-  const stack = new CdkEcrStack(app, 'ECRStack');
-  const template = Template.fromStack(stack);
+describe("ECR Stackのテスト", () => {
+  test("ECRが1つ作成されること", () => {
+    const app = new cdk.App();
+    const stack = new CdkEcrStack(app, "ECRStack");
+    const template = Template.fromStack(stack);
 
-  // 作成されるECRの数を確認する
-  template.resourceCountIs('AWS::ECR::Repository', 1);
-  // 作成されるECRがもつCloudFormationのPropertiesを確認する
-  template.hasResourceProperties('AWS::ECR::Repository', {
-    RepositoryName: 'my-ruby-app',
-    EmptyOnDelete: true,  // 中身があった場合でも削除する
-  });
-  // 作成されるECRのProperties以外の値を確認する
-  template.hasResource('AWS::ECR::Repository', {
-    DeletionPolicy: 'Delete', // スタック削除時にECRも削除する
+    // 作成されるECRの数を確認する
+    template.resourceCountIs("AWS::ECR::Repository", 1);
+    // 作成されるECRがもつCloudFormationのPropertiesを確認する
+    template.hasResourceProperties("AWS::ECR::Repository", {
+      RepositoryName: "my-ruby-app",
+      EmptyOnDelete: true, // 中身があった場合でも削除する
+    });
+    // 作成されるECRのProperties以外の値を確認する
+    template.hasResource("AWS::ECR::Repository", {
+      DeletionPolicy: "Delete", // スタック削除時にECRも削除する
+    });
   });
 });
