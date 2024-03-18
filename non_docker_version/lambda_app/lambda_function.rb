bc71b8da5ba013fdc3a1c953a2f95f9bc7337d37
Hidenori Miyoshi <miyohide@gmail.com>
2024-03-18 20:22:48 +0900

    pg gemだけをrequireするテスト

--- non_docker_version/lambda_app/lambda_function.rb
+++ non_docker_version/lambda_app/lambda_function.rb
@@ -1,10 +1,11 @@
-#require 'pg'
-require 'active_support/all'
+require 'pg'
+#require 'active_support/all'
 
 module LambdaFunction
   class Handler
     def self.process(event:,context:)
-      1.month.from_now
+      # 1.month.from_now
+      "Hello World"
     end
   end
 end
