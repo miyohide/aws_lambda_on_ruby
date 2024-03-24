566d2f02637c13171132200e4730b9b47caea1a9
Hidenori Miyoshi <miyohide@gmail.com>
2024-03-24 17:40:36 +0900

    コメントの整形

--- non_docker_version/lambda_app/lambda_function.rb
+++ non_docker_version/lambda_app/lambda_function.rb
@@ -24,10 +24,14 @@ module LambdaFunction
       # S3接続用のクライアントを作成
       client = Aws::S3::Client.new
       # バケット情報一覧を取得する
-      # #<struct Aws::S3::Types::ListBucketsOutput buckets=[#<struct Aws::S3::Types::Bucket name="名前", creation_date=日付>], owner=#<struct Aws::S3::Types::Owner display_name="xxxxx", id="yyyyy">>
+      # #<struct Aws::S3::Types::ListBucketsOutput
+      # buckets=[#<struct Aws::S3::Types::Bucket name="名前", creation_date=日付>],
+      # owner=#<struct Aws::S3::Types::Owner display_name="xxxxx", id="yyyyy">>
       p client.list_buckets
       # オブジェクト情報一覧
-      # #<struct Aws::S3::Types::ListObjectsV2Output is_truncated=false, contents=[#<struct Aws::S3::Types::Object key="名前", last_modified=日付, etag="\"xxxxx\"", checksum_algorithm=[], size=サイズ, storage_class="STANDARD", owner=nil, restore_status=nil>], name="bucket名", prefix="", delimiter=nil, max_keys=1000, common_prefixes=[], encoding_type=nil, key_count=1, continuation_token=nil, next_continuation_token=nil, start_after=nil, request_charged=nil>
+      # #<struct Aws::S3::Types::ListObjectsV2Output is_truncated=false,
+      # contents=[#<struct Aws::S3::Types::Object key="名前", last_modified=日付, etag="\"xxxxx\"", checksum_algorithm=[], size=サイズ, storage_class="STANDARD", owner=nil, restore_status=nil>],
+      # name="bucket名", prefix="", delimiter=nil, max_keys=1000, common_prefixes=[], encoding_type=nil, key_count=1, continuation_token=nil, next_continuation_token=nil, start_after=nil, request_charged=nil>
       objects = client.list_objects_v2(bucket: event["bucketname"])
       # S3からオブジェクトを/tmpにコピーする
       objects.contents.each do |content|
