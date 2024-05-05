05c6834aefcdf9acb54d11f53e586d12863f6328
Hidenori Miyoshi <miyohide@gmail.com>
2024-05-05 11:01:43 +0900

    update Ruby 3.3 version

--- non_docker_version_3.3/generate_layer/make_layer.sh
+++ non_docker_version_3.3/generate_layer/make_layer.sh
@@ -6,9 +6,9 @@ bundle install
 
 mkdir -p /var/task/lib
 
-cp -a /usr/lib64/libpq.so.5.14 /var/task/lib/libpq.so.5
-cp -a /usr/lib64/liblber-2.4.so.2.10.7 /var/task/lib/liblber-2.4.so.2
-cp -a /usr/lib64/libldap_r-2.4.so.2.10.7 /var/task/lib/libldap_r-2.4.so.2
+cp -a /usr/lib64/libpq.so.5.15 /var/task/lib/libpq.so.5
+cp -a /usr/lib64/liblber-2.4.so.2.11.5 /var/task/lib/liblber-2.4.so.2
+cp -a /usr/lib64/libldap_r-2.4.so.2.11.5 /var/task/lib/libldap_r-2.4.so.2
 cp -a /usr/lib64/libsasl2.so.3.0.0 /var/task/lib/libsasl2.so.3
 cp -a /usr/lib64/libssl3.so /var/task/lib/libssl3.so
 cp -a /usr/lib64/libsmime3.so /var/task/lib/libsmime3.so
